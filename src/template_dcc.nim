83a8e99b67459af7cf387d5f67c0f5819b86d797
tniyer2 <tanishqiyer88@gmail.com>
2021-09-25 21:40:58 -0400

    Created inputProc() to handle input rather than using setKeyCallback().   inputProc() runs once and polls GLFW for pressed keys.   setKeyCallback() invokes callback for each key.
    Initially set the size of the viewport.
    
    Using setFramebufferSizeCallback() to
      resize viewport
    
    Moved glfwPollEvents from the beginning of the frame to the end of the frame.
    
    Created type Color to store f64-rgba colors.
    
    Commented all the code (there were no comments before).

--- src/template_dcc.nim
+++ src/template_dcc.nim
@@ -2,37 +2,74 @@
 import nimgl/[glfw, opengl]
 
 
-proc keyProc(window: GLFWWindow, key: int32, scancode: int32,
-             action: int32, mods: int32): void {.cdecl.} =
-  if key == GLFWKey.ESCAPE and action == GLFWPress:
+type
+  Color = tuple[r: float64, g: float64, b: float64, a: float64]
+
+
+proc framebufferSizeProc(window: GLFWwindow, width: int32, height: int32
+                        ) : void {.cdecl.} = 
+    glViewport(0, 0, width, height)
+
+
+proc inputProc(window: GLFWWindow): void =
+  if window.getKey(GLFWKey.ESCAPE) == GLFWPress:
     window.setWindowShouldClose(true)
 
 
 proc main() =
+  # Initialize GLFW
   assert glfwInit()
-
   glfwWindowHint(GLFWContextVersionMajor, 3)
   glfwWindowHint(GLFWContextVersionMinor, 3)
-  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
   glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
-  glfwWindowHint(GLFWResizable, GLFW_FALSE)
+  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
+  glfwWindowHint(GLFWResizable, GLFW_TRUE)
 
-  let w: GLFWWindow = glfwCreateWindow(800, 600, "NimGL")
-  if w == nil:
-    quit(-1)
 
-  discard w.setKeyCallback(keyProc)
-  w.makeContextCurrent()
+  # Declare Constants
+  const
+    window_width: int32 = 800
+    window_height: int32 = 600
+    window_title: cstring = "Template DCC"
+    clear_color: Color = (0.68, 1.0, 0.34, 1.0)
 
+  # Create and Initialize a Window.
+  let window = block:
+    let w: GLFWWindow =
+      glfwCreateWindow(window_width, window_height, window_title)
+
+    if w == nil:
+      echo "Failed to create a GLFW window."
+      glfwTerminate()
+      quit(-1)
+
+    w.makeContextCurrent()
+  
+    discard w.setFramebufferSizeCallback(framebufferSizeProc)
+    w
+
+
+  # Loads OpenGL. Implemented by nimgl, doesn't use GLAD.
   assert glInit()
 
-  while not w.windowShouldClose:
-    glfwPollEvents()
-    glClearColor(0.68f, 1f, 0.34f, 1f)
-    glClear(GL_COLOR_BUFFER_BIT)
-    w.swapBuffers()
+  # Set the Dimensions of the Rendering Viewport
+  glViewport(0, 0, window_width, window_height)
 
-  w.destroyWindow()
+  # Run Game Loop
+  while not window.windowShouldClose:
+    # Run Frame
+    block:
+      inputProc(window) # Process Input
+
+      # Set Background Color and Display
+      glClearColor(clear_color.r, clear_color.g, clear_color.b, clear_color.a)
+      glClear(GL_COLOR_BUFFER_BIT)
+      window.swapBuffers()
+
+      glfwPollEvents() # Process Events Sent from OS
+
+  # Clean Up GLFW
+  window.destroyWindow()
   glfwTerminate()
 
 
