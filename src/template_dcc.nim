b6339421a202743b41f09bf6fb84055b7d608990
tniyer2 <tanishqiyer88@gmail.com>
2021-09-27 00:24:36 -0400

    Before this commit the program was only clearing the screen with a color.
    Now the program is able to render vertices.
      It is currently rendering two triangles.
    
    Major Changes:
    
    Added code to load, compile, and link the vertex and fragment shaders.
        Also added code to 'echo' any compilation and linking errors.
    
    Added directory "shaders" to store shader files.
        Created default vertex shader at "shaders/default.vs".
        Created default fragment shader at "shaders/default.fs".
    
    Created 'Mesh' type.
      'proc loadMeshIntoOpenGL'
        Sets up a VAO and vertex attributes.
        Loads a vertex array into OpenGL.
      'proc drawTriangles'
        Draws the triangles in the meshes.
      'proc destroy'
        Destroys all OpenGL resources for this mesh (VAO, VBO).
    
    Minor Changes:
    
    Fields in type 'Color' changed from 'float64' to 'GLfloat' ('float32').
    
    Added numeric suffixes everywhere for clarity.

--- src/template_dcc.nim
+++ src/template_dcc.nim
@@ -2,15 +2,13 @@
 import nimgl/[glfw, opengl]
 
 
-type
-  Color = tuple[r: float64, g: float64, b: float64, a: float64]
+type Color = tuple[r, g, b, a: GLfloat]
 
 
 proc framebufferSizeProc(window: GLFWwindow, width: int32, height: int32
                         ) : void {.cdecl.} =
-    # Resize Viewport
-    glViewport(0, 0, width, height)
-
+  # Resize Viewport
+  glViewport(0, 0, width, height)
 
 proc inputProc(window: GLFWWindow): void =
   # Close Window if Esc.
@@ -18,24 +16,90 @@ proc inputProc(window: GLFWWindow): void =
     window.setWindowShouldClose(true)
 
 
+proc loadAndCompileShader(path: string, type_of_shader: GLenum): GLuint =
+  # Read shader source code from File
+  var source: cstring = readFile(path)
+
+  # Load the shader into OpenGL
+  let id = glCreateShader(type_of_shader)
+  glShaderSource(id, 1'i32, addr(source), nil)
+
+  # Compile the shader
+  glCompileShader(id)
+
+  # Report any Compilation Errors
+  var success: GLint
+  glGetShaderiv(id, GL_COMPILE_STATUS, addr(success))
+  if success != ord(GL_TRUE):
+    const log_size: int32 = 1024
+    var log: array[log_size, char]
+    glGetShaderInfoLog(id, log_size, nil, addr(log));
+
+    let s: string =
+      case type_of_shader
+        of GL_VERTEX_SHADER: "VERTEX"
+        of GL_FRAGMENT_SHADER: "FRAGMENT"
+        else: "UNKNOWN"
+    echo "ERROR::SHADER::" & s & "::COMPILATION_FAILED\n"
+    echo cast[cstring](addr(log))
+  
+  id
+
+
+type Mesh = tuple[vao, vbo: GLuint, length: int]
+
+proc loadMeshIntoOpenGL(address: pointer, length: int): Mesh =
+  assert length mod 3 == 0
+
+  var mesh: Mesh = (0'u32, 0'u32, length)
+
+  glGenVertexArrays(1'i32, addr(mesh.vao))
+  glGenBuffers(1'i32, addr(mesh.vbo))
+
+  glBindVertexArray(mesh.vao)
+
+  glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo)
+  glBufferData(GL_ARRAY_BUFFER, length * sizeof(float32) * 3, address, GL_STATIC_DRAW)
+
+  glVertexAttribPointer(0'u32, 3'i32, EGL_FLOAT, false, sizeof(GLfloat) * 3, nil)
+  glEnableVertexAttribArray(0'u32);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0'u32)
+  glBindVertexArray(0'u32)
+
+  mesh
+
+proc drawTriangles(mesh: var Mesh) =
+  glBindVertexArray(mesh.vao)
+  glDrawArrays(GL_TRIANGLES, 0'i32, cast[GLsizei](mesh.length div 3'i32))
+
+proc destroy(mesh: var Mesh) =
+  glDeleteVertexArrays(1'i32, addr(mesh.vao))
+  glDeleteBuffers(1'i32, addr(mesh.vbo))
+
+
 proc main() =
-  # Initialize GLFW
-  assert glfwInit()
-  glfwWindowHint(GLFWContextVersionMajor, 3)
-  glfwWindowHint(GLFWContextVersionMinor, 3)
-  glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
-  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
-  glfwWindowHint(GLFWResizable, GLFW_TRUE)
-
-
   # Declare Constants
   const
     window_width: int32 = 800
     window_height: int32 = 600
     window_title: cstring = "Template DCC"
-    clear_color: Color = (0.68, 1.0, 0.34, 1.0)
+    clear_color: Color = (0.68'f32, 1.0'f32, 0.34'f32, 1.0'f32)
+    vertex_shader_path: string = "shaders/default.vs"
+    fragment_shader_path: string = "shaders/default.fs"
 
-  # Create and Initialize a Window.
+
+  # Initialize GLFW
+  block:
+    assert glfwInit()
+    glfwWindowHint(GLFWContextVersionMajor, 3'i32)
+    glfwWindowHint(GLFWContextVersionMinor, 3'i32)
+    glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
+    glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
+    glfwWindowHint(GLFWResizable, GLFW_TRUE)
+
+
+  # Create and Initialize a Window
   let window = block:
     let w: GLFWWindow =
       glfwCreateWindow(window_width, window_height, window_title)
@@ -51,11 +115,51 @@ proc main() =
     w
 
 
-  # Loads OpenGL. Implemented by nimgl, doesn't use GLAD.
+  # Load OpenGL. Implemented by nimgl, doesn't use GLAD.
   assert glInit()
 
-  # Set the Dimensions of the Rendering Viewport
-  glViewport(0, 0, window_width, window_height)
+
+  # Load, Compile, and Link the Vertex and Fragment Shaders
+  let po = block:
+    # Load and Compile Shaders
+    let vso = loadAndCompileShader(vertex_shader_path, GL_VERTEX_SHADER)
+    let fso = loadAndCompileShader(fragment_shader_path, GL_FRAGMENT_SHADER)
+
+    # Link the Shader Objects
+    let po = glCreateProgram()
+    glAttachShader(po, vso)
+    glAttachShader(po, fso)
+    glLinkProgram(po)
+
+    # Report any Linking Errors
+    var success: GLint
+    glGetProgramiv(po, GL_LINK_STATUS, addr(success))
+    if success != ord(GL_TRUE):
+      const log_size: int32 = 1024
+      var log: array[log_size, char]
+      glGetProgramInfoLog(po, log_size, nil, addr(log))
+
+      echo "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+      echo cast[cstring](addr(log))
+
+    # Delete the Shader Objects
+    glDeleteShader(vso)
+    glDeleteShader(fso)
+    po
+
+
+  # Set Up Vertex Data
+  var mesh = block:
+    var vertices = [
+      -0.5'f32, -0.5'f32,  0.0'f32,
+       0.5'f32, -0.5'f32,  0.0'f32,
+       0.0'f32,  0.5'f32,  0.0'f32,
+       0.0'f32, -0.75'f32, 0.0'f32,
+       0.5'f32,  0.25'f32, 0.0'f32,
+      -0.5'f32,  0.25'f32, 0.0'f32
+    ]
+    loadMeshIntoOpenGL(addr(vertices), len(vertices))
+
 
   # Run Game Loop
   while not window.windowShouldClose:
@@ -66,9 +170,17 @@ proc main() =
       # Set Background Color and Display
       glClearColor(clear_color.r, clear_color.g, clear_color.b, clear_color.a)
       glClear(GL_COLOR_BUFFER_BIT)
+
+      glUseProgram(po)
+      mesh.drawTriangles()
+
       window.swapBuffers()
 
       glfwPollEvents() # Process Events Sent from OS
+
+  # Clean Up OpenGL
+  mesh.destroy()
+  glDeleteProgram(po)
 
   # Clean Up GLFW
   window.destroyWindow()
